bc5dc096f6792af10e372086502a13bfe49d03f6
AWAUOX <awauox@outlook.com>
2026-05-05 18:50:45 +0800

    继续测试多个同步预加载与取消预加载的问题

--- demo_test_scene_manager/test_scene_2_script/test_scene_2_gd.gd
+++ demo_test_scene_manager/test_scene_2_script/test_scene_2_gd.gd
@@ -247,15 +247,20 @@ func _on_remove_cached_scene_5_pressed():
 
 
 func _on_preload_all_pressed() -> void:
+	set_process(true)
 	LongSceneManager.preload_scenes([TEST_SCENE_3_PATH,TEST_SCENE_4_PATH,TEST_SCENE_5_PATH])
 	#pass # Replace with function body.
 	_update_info()
 
 func _on_cancel_all_preload_pressed() -> void:
+	set_process(true)
 	LongSceneManager.cancel_all_preloads()
+	_update_info()
 
 
 
 func _on_cancel_preload_scene_3_pressed() -> void:
+	set_process(true)
 	LongSceneManager.cancel_preload_scene(TEST_SCENE_3_PATH)
+	_update_info()
 	
